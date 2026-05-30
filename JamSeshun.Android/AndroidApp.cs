aa82fcaebad5d56f5d9ad135788dcd799f425455
Andrew Chisholm <flatlinerdoa@gmail.com>
2026-05-30 22:36:33 +1000

    Android cleanup

---
+++ JamSeshun.Android/AndroidApp.cs
@@ -0,0 +1,37 @@
+﻿namespace JamSeshun.Android;
+
+using System;
+using Avalonia;
+using Avalonia.Android;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using global::Android.App;
+using global::Android.Runtime;
+using Microsoft.Extensions.DependencyInjection;
+using Optris.Icons.Avalonia;
+using Optris.Icons.Avalonia.FontAwesome;
+using Services;
+using Services.Tuning;
+
+[Application]
+public class AndroidApp : AvaloniaAndroidApplication<App>
+{
+    public AndroidApp(IntPtr javaReference, JniHandleOwnership transfer)
+        : base(javaReference, transfer) { }
+
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        IconProvider.Current.Register<FontAwesomeIconProvider>();
+        return base.CustomizeAppBuilder(builder)
+            .ConfigureServices(services =>
+            {
+                ServiceCollectionServiceExtensions.AddSingleton<ITuningService, AndroidTuningService>(services);
+            })
+            .WithInterFont()
+            .AfterSetup(_ =>
+            {
+                if (App.Current?.ApplicationLifetime is ISingleViewApplicationLifetime singleView)
+                    singleView.MainView = App.ServiceProvider.GetKeyedService<Control>("MainView");
+            });
+    }
+}
--- JamSeshun.Android/MainActivity.cs
+++ JamSeshun.Android/MainActivity.cs
@@ -1,18 +1,8 @@
-using System;
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
-using Android.Runtime;
 using AndroidX.Core.App;
-using Avalonia;
 using Avalonia.Android;
-using Avalonia.Controls;
-using Avalonia.Controls.ApplicationLifetimes;
-using JamSeshun.Services;
-using JamSeshun.Services.Tuning;
-using Microsoft.Extensions.DependencyInjection;
-using Optris.Icons.Avalonia;
-using Optris.Icons.Avalonia.FontAwesome;
 
 namespace JamSeshun.Android;
 
@@ -24,7 +14,7 @@ namespace JamSeshun.Android;
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
 public class MainActivity : AvaloniaMainActivity
 {
-    protected override void OnCreate(Bundle savedInstanceState)
+    protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
         Xamarin.Essentials.Platform.Init(this, savedInstanceState);
@@ -34,28 +24,5 @@ public class MainActivity : AvaloniaMainActivity
     {
         Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-}
-
-[Application]
-public class AndroidApp : AvaloniaAndroidApplication<App>
-{
-    public AndroidApp(IntPtr javaReference, JniHandleOwnership transfer)
-        : base(javaReference, transfer) { }
-
-    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
-    {
-        IconProvider.Current.Register<FontAwesomeIconProvider>();
-        return base.CustomizeAppBuilder(builder)
-            .ConfigureServices(services =>
-            {
-                services.AddSingleton<ITuningService, AndroidTuningService>();
-            })
-            .WithInterFont()
-            .AfterSetup(_ =>
-            {
-                if (App.Current?.ApplicationLifetime is ISingleViewApplicationLifetime singleView)
-                    singleView.MainView = App.ServiceProvider.GetKeyedService<Control>("MainView");
-            });
     }
 }
